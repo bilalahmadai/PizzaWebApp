84000c50c2233071e5b94f8c0a03138582e6744e
bilalansar3 <bilalahmadai971@gmail.com>
2022-12-05 14:29:05 +0500

    DBprice added (add-migration,update-database)

--- PizzaWebApp/Data/Migrations/20221205091818_PizzaEntityUpdatePrice.Designer.cs
+++ PizzaWebApp/Data/Migrations/20221205091818_PizzaEntityUpdatePrice.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaWebApp.Data;
 
 namespace PizzaWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221205091818_PizzaEntityUpdatePrice")]
+    partial class PizzaEntityUpdatePrice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
