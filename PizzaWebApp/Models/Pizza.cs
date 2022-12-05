5b94cc9f3dd90c40a9e43cfcc76fee2eff059398
bilalansar3 <bilalahmadai971@gmail.com>
2022-12-05 10:29:05 +0500

    Add project files.

---
+++ PizzaWebApp/Models/Pizza.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaWebApp.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Desc { get; set; }
+        //[Required]  after authentication process update dataBase
+        //public int Price { get; set; }
+        [Required]
+        public string Url { get; set; }
+    }
+}
+
+/*
+ * form with MVC
+ * search form
+ * Authentication
+ * update price M DB V 
+ * entity relation with new MODEL (cheff)
+ * virtual to refrence to anther table like cheff to Pizza not a table in this Model
+ * 
+ 
+ */
