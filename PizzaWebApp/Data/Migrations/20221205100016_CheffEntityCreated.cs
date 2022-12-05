fe378381e08af61af26df0a6d39c19c4785fc78a
bilalansar3 <bilalahmadai971@gmail.com>
2022-12-05 15:24:13 +0500

    realtion  Cheff model id   as  Fid to Pizza Model

---
+++ PizzaWebApp/Data/Migrations/20221205100016_CheffEntityCreated.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaWebApp.Data.Migrations
+{
+    public partial class CheffEntityCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cheff",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cheff_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cheff", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cheff");
+        }
+    }
+}
---
+++ PizzaWebApp/Data/Migrations/20221205100940_pizzaCheffRelationCreated.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaWebApp.Data.Migrations
+{
+    public partial class pizzaCheffRelationCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CheffFID",
+                table: "Pizza",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizza_CheffFID",
+                table: "Pizza",
+                column: "CheffFID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizza_Cheff_CheffFID",
+                table: "Pizza",
+                column: "CheffFID",
+                principalTable: "Cheff",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pizza_Cheff_CheffFID",
+                table: "Pizza");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pizza_CheffFID",
+                table: "Pizza");
+
+            migrationBuilder.DropColumn(
+                name: "CheffFID",
+                table: "Pizza");
+        }
+    }
+}
